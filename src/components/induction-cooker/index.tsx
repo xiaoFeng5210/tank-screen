f650e7a6837d08c9446eda4fd05791989cf32319
Eadweard <aurora.coder.zqf@gmail.com>
2024-05-15 15:57:24 +0800

    finish

--- src/components/induction-cooker/index.tsx
+++ src/components/induction-cooker/index.tsx
@@ -1,20 +1,47 @@
 import './index.scss'
 import {InductionCookerStatus} from "~/types/induction-cooker.ts";
 import {FC, useMemo} from "react";
+import { motion } from "framer-motion"
 
 interface InductionCookerProps {
   status: InductionCookerStatus
 }
 
-const InductionCooker: FC<InductionCookerProps> = ({status = InductionCookerStatus.IDLE}) => {
+const heatingAnimate={
+  scale: [1, 0.9, 0.9, 1, 1],
+  rotate: [0, 0, 270, 270, 0],
+  borderRadius: ["20%", "20%", "50%", "50%", "20%"],
+}
+const heatingTransition={
+  repeat: Infinity, // 无限循环
+  duration: 2, // 动画时长
+}
 
+const completedAnimate = {
+  scale: [1,1.1, 1, 0.9],
+  opacity: [1, 0.5, 0.2, 0.5],
+}
+
+const completedTransition = {
+  repeat: Infinity,
+  duration: 1,
+}
+
+const haspotAnimate = {
+  opacity: [1, 0, 1],
+}
+const haspotTransition = {
+  repeat: Infinity,
+  duration: 2.5,
+}
+
+const InductionCooker: FC<InductionCookerProps> = ({status = InductionCookerStatus.IDLE}) => {
   const statusMatch = useMemo(() => {
-    console.log(status)
     if (status === InductionCookerStatus.IDLE) return <div className="w-[100px] h-[100px] bg-white rounded-full"></div>
-    if (status === InductionCookerStatus.HEATING) return <div className="w-[100px] h-[100px] bg-[#FF0212] rounded-full"></div>
-    if (status === InductionCookerStatus.HAS_POT) return <div className="w-[100px] h-[100px] bg-[#4F81BD] rounded-full"></div>
+    if (status === InductionCookerStatus.HEATING) return <motion.div animate={heatingAnimate} transition={heatingTransition} className="w-[100px] h-[100px] bg-[#FF0212] rounded-full"></motion.div>
+    if (status === InductionCookerStatus.HAS_POT) return <motion.div animate={haspotAnimate} transition={haspotTransition} className="w-[100px] h-[100px] bg-[#4F81BD] rounded-full"></motion.div>
     if (status === InductionCookerStatus.ERROR) return <div className="w-[100px] h-[100px] rounded-full"></div>
-    if (status === InductionCookerStatus.Completed) return <div className="w-[100px] h-[100px] bg-[#9BBB59] rounded-full"></div>
+    if (status === InductionCookerStatus.Completed) return <motion.div animate={completedAnimate} transition={completedTransition} className="w-[100px] h-[100px] bg-[#9BBB59] rounded-full"></motion.div>
     return <div className="w-[100px] h-[100px] bg-white rounded-full"></div>
   }, [status])
 
